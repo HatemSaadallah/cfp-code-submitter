ab8b90fa3282d678ae2c797f8f462d764616b9da
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-03 13:07:09 +0300

    updated readme

---
+++ precache-manifest.763b045d99f300f2a51ccebf009d7bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0451173371053a28c9838bd55bd087f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "563629c1f513e6f23113",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "536715b5678efa7a21e5",
+    "url": "/static/css/main.37309482.chunk.css"
+  },
+  {
+    "revision": "563629c1f513e6f23113",
+    "url": "/static/js/2.bde966ba.chunk.js"
+  },
+  {
+    "revision": "36d2426f7924d47ec9dd8f63641be153",
+    "url": "/static/js/2.bde966ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536715b5678efa7a21e5",
+    "url": "/static/js/main.19d18486.chunk.js"
+  },
+  {
+    "revision": "938384f67e1de8c9e588",
+    "url": "/static/js/runtime-main.2ce565f7.js"
+  }
+]);
