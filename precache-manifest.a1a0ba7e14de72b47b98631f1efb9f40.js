f2592427f900a606cbc8eefa9bc7a1807c8ecb17
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-03 03:14:13 +0300

    deployed successfully

---
+++ precache-manifest.a1a0ba7e14de72b47b98631f1efb9f40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34342102de198c1c96f6140412871019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7161ae781edad4bffad",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2309820675326d918973",
+    "url": "/static/css/main.d5c63dad.chunk.css"
+  },
+  {
+    "revision": "a7161ae781edad4bffad",
+    "url": "/static/js/2.86998c5b.chunk.js"
+  },
+  {
+    "revision": "e73ace63a90d9f5749945711a30f0ff0",
+    "url": "/static/js/2.86998c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2309820675326d918973",
+    "url": "/static/js/main.d60efc7b.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
