0fbaf570a90b4ace9a894dfd834b1dd7db651e2a
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-07 23:57:45 +0300

    more coloring fixing

---
+++ build/precache-manifest.eee44f9766b004d87ceaf1bc0b6a3676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0f49c39d9a5c44eceb8401c34c58fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05911c1e596318a0d197",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d6b37988508d99833a86",
+    "url": "/static/css/main.3f65720b.chunk.css"
+  },
+  {
+    "revision": "05911c1e596318a0d197",
+    "url": "/static/js/2.da11c6e1.chunk.js"
+  },
+  {
+    "revision": "36d2426f7924d47ec9dd8f63641be153",
+    "url": "/static/js/2.da11c6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b37988508d99833a86",
+    "url": "/static/js/main.42cb2706.chunk.js"
+  },
+  {
+    "revision": "938384f67e1de8c9e588",
+    "url": "/static/js/runtime-main.2ce565f7.js"
+  }
+]);
