4322ead80ab38765d7a1510d561715b37e6c02f7
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-07 22:27:17 +0300

    Activated Hot Reload

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-
+if(module.hot){
+    module.hot.accept()
+  }
 ReactDOM.render(<App />, document.getElementById("root"));
