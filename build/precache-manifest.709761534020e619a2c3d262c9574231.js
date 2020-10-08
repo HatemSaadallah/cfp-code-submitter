478f99e1f695bd5aa44470084490c22235411cc0
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-08 20:23:06 +0300

    fixed embeded tab coloring

---
+++ build/precache-manifest.709761534020e619a2c3d262c9574231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d5af5ffff3f684c27fbe172ef42aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95a8c80ebe85a7220d01",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "21d7bfbc81ab3e73768a",
+    "url": "/static/css/main.ab1cb46d.chunk.css"
+  },
+  {
+    "revision": "95a8c80ebe85a7220d01",
+    "url": "/static/js/2.4e2ce466.chunk.js"
+  },
+  {
+    "revision": "36d2426f7924d47ec9dd8f63641be153",
+    "url": "/static/js/2.4e2ce466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d7bfbc81ab3e73768a",
+    "url": "/static/js/main.fcf386ec.chunk.js"
+  },
+  {
+    "revision": "938384f67e1de8c9e588",
+    "url": "/static/js/runtime-main.2ce565f7.js"
+  }
+]);
