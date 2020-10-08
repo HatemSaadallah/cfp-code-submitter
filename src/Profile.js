af76237b109beadc71fae8d7cb87c20a21435314
Mohammed Atalah <68267242+Mohammed-Atalah@users.noreply.github.com>
2020-10-08 22:24:45 +0300

    Update profile
    make the sign out on the profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styling/styles.css';
 import './styling/profileStyles.css';
+import firebase from "firebase";
+
 
 
 export default function Profile({ imageURL, name }) {
@@ -11,6 +13,10 @@ export default function Profile({ imageURL, name }) {
 
             <img className="profileImage" src={imageURL} />
             <h1>{name}</h1>
+            <button onClick={() => firebase.auth().signOut()} className="sign-out-button"> Sign out  </button>
+
         </div>
+            
+           
     );
 }
