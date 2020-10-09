3686c74d32092130a1a4a864a2f2846aa8bff12a
Mohammed Atalah <68267242+Mohammed-Atalah@users.noreply.github.com>
2020-10-09 07:49:26 +0300

    Add a class to offichours.js
    I added a CSS class so I can make changes in the style easier.

--- src/OfficeHours.js
+++ src/OfficeHours.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './styling/styles.css';
 export default function OfficeHours(){
     return(
-        <div className="container">
-            <h1>Welcome, Student.</h1>
-            <hr></hr>
+        <div className="office-hours-container">
+            
             <h2>Office Hours:</h2>
             <iframe className="office-hours-sheet" src="https://sheet2api.com/table/o9tywNtETM2Q/cfp-y1-office-hours/Week2"
             width="100%" height="600" frameBorder="0"></iframe>
