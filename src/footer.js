1728b290f2ef07e2346e5670113af88fc9ec4ab0
Mohammed Atalah <68267242+Mohammed-Atalah@users.noreply.github.com>
2020-10-09 14:05:29 +0300

    Add extra links to the footer
    Add CFP, W3schools, Python documentation, Kahoot links to the footer

--- src/footer.js
+++ src/footer.js
@@ -10,7 +10,14 @@ const Footer = () => (
     <p><a href="https://www.facebook.com/messages/t/HeatoMu">Hatem</a></p>
     <p><a href="https://www.facebook.com/messages/t/hanaa.zaqout.351">Hanaa</a></p>
     <p><a href="https://www.facebook.com/messages/t/abdullah.baraka.5">Abdullah</a></p>
-    <p><a href="https://www.facebook.com/t/zayan.hassan.1">Zayan</a></p>
+    <p><a href="https://www.facebook.com/messages/t/zayan.hassan.1">Zayan</a></p>
+    <br></br>
+    <p>Links |</p>
+    <p><a href="http://www.codeforpalestine.ps/">Code For Palestine</a></p>
+    <p><a href="https://www.w3schools.com/python/python_getstarted.asp">W3schools</a></p>
+    <p><a href="https://docs.python.org/3.8/">Python documentation</a></p>
+    <p><a href="https://kahoot.it/">Kahout</a></p>
+
     <button onClick={() => firebase.auth().signOut()} className="sign-out-button"> Sign out  </button>
   </div>
 );
