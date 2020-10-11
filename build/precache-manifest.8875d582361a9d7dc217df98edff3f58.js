ba41b598bcb769e4237e137be2bf622dfbedda79
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-11 03:30:25 +0300

    fixed border radius

---
+++ build/precache-manifest.8875d582361a9d7dc217df98edff3f58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8293351084f419177cb392c7c6d8e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6bacc407dedc52658de",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "882f6642ae3beadd52a8",
+    "url": "/static/css/main.1aa69348.chunk.css"
+  },
+  {
+    "revision": "c6bacc407dedc52658de",
+    "url": "/static/js/2.08601abd.chunk.js"
+  },
+  {
+    "revision": "36d2426f7924d47ec9dd8f63641be153",
+    "url": "/static/js/2.08601abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882f6642ae3beadd52a8",
+    "url": "/static/js/main.27b8eb6b.chunk.js"
+  },
+  {
+    "revision": "938384f67e1de8c9e588",
+    "url": "/static/js/runtime-main.2ce565f7.js"
+  }
+]);
