847a59b9d86f53540bb657cf74af36b41541824f
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2020-10-13 04:05:03 +0300

    commit on master

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,24 +1,23 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import firebase from "firebase";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Dropdown } from "react-bootstrap";
-import { NavLink } from 'react-router-dom'
-
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import firebase from 'firebase';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAW7-NGHjYyEOUWZL9eIvSp-glELUXuq0E",
-  authDomain: "inclass-qs.firebaseapp.com",
-  databaseURL: "https://inclass-qs.firebaseio.com",
-  projectId: "inclass-qs",
-  storageBucket: "inclass-qs.appspot.com",
-  messagingSenderId: "76230869433",
-  appId: "1:76230869433:web:de34cca5ec6ecc92457668",
-  measurementId: "G-151K89PMKH"
+  apiKey: 'AIzaSyAW7-NGHjYyEOUWZL9eIvSp-glELUXuq0E',
+  authDomain: 'inclass-qs.firebaseapp.com',
+  databaseURL: 'https://inclass-qs.firebaseio.com',
+  projectId: 'inclass-qs',
+  storageBucket: 'inclass-qs.appspot.com',
+  messagingSenderId: '76230869433',
+  appId: '1:76230869433:web:de34cca5ec6ecc92457668',
+  measurementId: 'G-151K89PMKH',
 };
 try {
   firebase.initializeApp(firebaseConfig);
-} catch { }
+} catch {}
 
 export default function NavBar({ name, signedIn, isSuperUser = false }) {
   return (
@@ -27,46 +26,51 @@ export default function NavBar({ name, signedIn, isSuperUser = false }) {
         <Nav.Link href="/main">Code for Palestine Online Submitter</Nav.Link>
       </Navbar.Brand>
       <Navbar.Toggle />
-      {(signedIn) ?
+      {signedIn ? (
         <div class="userNav">
-          <NavLink className="NavLink" exact to="/pastproblems">Past Problems</NavLink>
-          <NavLink className="NavLink" exact to="/gists">Gists</NavLink>
-          <NavLink className="NavLink" exact to="/previous-submissions">Previous Submissions</NavLink>
-          <NavLink className="NavLink" exact to="/office-hours">Sessions</NavLink>
-          <NavLink className="NavLink" exact to="/TAs_info">TAs</NavLink>
+          <NavLink className="NavLink" exact to="/pastproblems">
+            Past Problems
+          </NavLink>
+          <NavLink className="NavLink" exact to="/gists">
+            Gists
+          </NavLink>
+          <NavLink className="NavLink" exact to="/previous-submissions">
+            Previous Submissions
+          </NavLink>
+          <NavLink className="NavLink" exact to="/office-hours">
+            Sessions
+          </NavLink>
+          <NavLink className="NavLink" exact to="/TAs_info">
+            TAs
+          </NavLink>
 
-          {(isSuperUser) ?
-            <NavLink to="/admin">Admin Panel</NavLink>
-            :
-            null
-          }
+          {isSuperUser ? <NavLink to="/admin">Admin Panel</NavLink> : null}
         </div>
-        :
-        null
-      }
-
+      ) : null}
 
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
-
-          {(signedIn) ? null: <a>You are not signed in</a>}
+          {signedIn ? null : <a>You are not signed in</a>}
           {signedIn ? (
             <div className="signedIn">
-
               {/* <Dropdown> */}
-              
-                {/* <Dropdown.Toggle id="dropdown-basic" className="profile"> */}
-                  <Nav.Link href="profile"><a className="nameUser">{name}</a></Nav.Link>
-                  <img className="profileImg" src={firebase.auth().currentUser.photoURL} />
-                {/* </Dropdown.Toggle> */}
 
-                {/* <Dropdown.Menu>
+              {/* <Dropdown.Toggle id="dropdown-basic" className="profile"> */}
+              <NavLink to="profile">
+                <a className="nameUser">{name}</a>
+              </NavLink>
+              <img
+                className="profileImg"
+                src={firebase.auth().currentUser.photoURL}
+              />
+              {/* </Dropdown.Toggle> */}
+
+              {/* <Dropdown.Menu>
                   
                   <Dropdown.Item 
                     onClick={() => firebase.auth().signOut()}>Sign Out</Dropdown.Item>
                 </Dropdown.Menu> */}
               {/* </Dropdown> */}
-
             </div>
           ) : null}
         </Navbar.Text>
--- src/Profile.js
+++ src/Profile.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './styling/styles.css';
 import './styling/profileStyles.css';
-
-
+import { Button } from 'react-bootstrap';
+import { BiEdit } from 'react-icons/bi';
+import { ImCheckmark } from 'react-icons/im';
 
 export default function Profile({ imageURL, name }) {
-    return (
-        <div>
-        <div className="container">
-            <head>
-            </head>
+  function editAble() {
+    document.getElementById('breif').contentEditable = 'true';
+    document.getElementById('saveBtn').style.display = 'unset';
+  }
+  function editUnAble() {
+    document.getElementById('breif').contentEditable = 'false';
+    document.getElementById('saveBtn').style.display = 'none';
+    console.log('brief changed.');
+  }
 
-            <img className="profileImage" src={imageURL} />
-            <h1>{name}</h1>
-            
-            
-        </div>
-        
-        </div>
-            
-           
-    );
+  return (
+    <div>
+      <div className="container">
+        <head></head>
+
+        <img className="profileImage" src={imageURL} />
+        <h1 id="username" className="profile_name">
+          {name}
+        </h1>
+        <p className="breif" id="breif">
+          add your brief here :3
+        </p>
+        <Button onClick={editAble}>
+          <BiEdit />
+        </Button>
+        <Button id="saveBtn" onClick={editUnAble}>
+          <ImCheckmark />
+        </Button>
+      </div>
+    </div>
+  );
 }
