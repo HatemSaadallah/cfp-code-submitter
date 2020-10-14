b6a8ee6a71102713ef13b39bcac75da80b577350
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-14 22:36:45 +0300

    Fixed gists

--- src/Gists.js
+++ src/Gists.js
@@ -6,24 +6,24 @@ import { gists } from './data/gistsLinks';
 export default function Gists() {
   return (
     <div>
-      {gists.map((gist) => {
-        return (
-          <div>
-            <Accordion className="gists-card" >
-              <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="0">
-                  {gist["gistName"]}
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Gist url={gist["url"]}
-                    file={gist["fileName"]} />
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
+      {gists.map((gist) => (
+        <div>
+          <Accordion className="gists-card">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                {gist.gistName}
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Gist
+                  url={gist.url}
+                  file={gist.fileName}
+                />
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
 
-          </div>
-        );
-      })}
+        </div>
+      ))}
 
     </div>
   );
