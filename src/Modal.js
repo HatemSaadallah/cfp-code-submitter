33afefe58d9ccc0b671bb21be04d996b7384a559
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-14 23:04:14 +0300

    Linting Navbar.js

--- src/Modal.js
+++ src/Modal.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Modal, Button } from "react-bootstrap";
+import PropTypes from 'prop-types';
+import { Modal, Button } from 'react-bootstrap';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-
-function MyVerticallyCenteredModal(props) {
+function MyVerticallyCenteredModal({
+  show,
+  onHide,
+  code,
+  nameOfQuestion,
+}) {
   return (
     <Modal
-      {...props}
+      show={show}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
@@ -15,22 +20,34 @@ function MyVerticallyCenteredModal(props) {
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
           Question Submitted Successfully
-          </Modal.Title>
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4 className="question_name">{props.nameOfQuestion}</h4>
+        <h4 className="question_name">{nameOfQuestion}</h4>
         <p>
 
           <SyntaxHighlighter language="python" style={docco}>
-            {props.code}
+            {code}
           </SyntaxHighlighter>
         </p>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
 }
+MyVerticallyCenteredModal.defaultProps = {
+  show: false,
+  onHide: null,
+  code: '',
+  nameOfQuestion: '',
+};
 
+MyVerticallyCenteredModal.propTypes = {
+  show: PropTypes.bool,
+  onHide: PropTypes.func,
+  code: PropTypes.string,
+  nameOfQuestion: PropTypes.string,
+};
 export default MyVerticallyCenteredModal;
