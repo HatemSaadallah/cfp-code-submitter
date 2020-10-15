a898da3435314e9aa8b503cdd2814b4646207c79
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-15 22:52:25 +0300

    eslinting Questions.js

--- src/Questions.js
+++ src/Questions.js
@@ -1,26 +1,26 @@
-import React from "react";
-import "./styling/styles.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styling/styles.css';
 import ReactMarkdown from 'react-markdown';
-import { Accordion, Card } from "react-bootstrap";
+import { Accordion, Card } from 'react-bootstrap';
 
 export default function Questions({ questions, submissions }) {
   return (
-    
     <div className="questions">
-      {/* <a href="/casual_code" className="a_casual_code_btn" >
-        <button type="button" className="casual_code_btn"> Submit Your Code Now</button>
+      {/* <a href='/casual_code' className='a_casual_code_btn' >
+        <button type='button' className='casual_code_btn'> Submit Your Code Now</button>
       </a>  */}
       {questions.map(question => {
         return (
           <div>
-            <Accordion defaultActiveKey="1">
+            <Accordion defaultActiveKey='1'>
               
               <Card>
-                <Accordion.Toggle as={Card.Header} eventKey={question["num"]}>
-                <h3>Q{question["num"]}- {question["qName"]}</h3>
+                <Accordion.Toggle as={Card.Header} eventKey={question['num']}>
+                <h3>Q{question['num']}- {question['qName']}</h3>
                 </Accordion.Toggle>
-                <Accordion.Collapse eventKey={question["num"]}>
-                <p><ReactMarkdown source={question["content"]} /></p>
+                <Accordion.Collapse eventKey={question['num']}>
+                <p><ReactMarkdown source={question['content']} /></p>
                 </Accordion.Collapse>
               </Card>
             </Accordion>
@@ -31,3 +31,12 @@ export default function Questions({ questions, submissions }) {
     </div>
   );
 }
+Questions.defaultProps = {
+  questions: '',
+  submissions: '',
+};
+
+Questions.propTypes = {
+  questions: PropTypes.string,
+  submissions: PropTypes.string,
+};
