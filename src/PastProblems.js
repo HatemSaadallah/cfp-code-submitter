8278804204610d48718c719435cbcc6232dbd608
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-10-15 16:47:19 +0300

    Past Problems done

--- src/PastProblems.js
+++ src/PastProblems.js
@@ -1,53 +1,51 @@
 import React from 'react';
-import { pastproblems } from './data/weeklyQuestions';
-import { Table } from 'react-bootstrap';
+import { Accordion, Card, Table } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
-import "./styling/styles.css";
-import { Accordion, Card } from "react-bootstrap";
+import { pastproblems } from './data/weeklyQuestions';
+import './styling/styles.css';
 
 export default function PastProblems() {
-
   return (
     <div>
-      { pastproblems.map(week => {
-        return (
-          <div>
-              <Accordion className="problems-card" >
+      { pastproblems.map((week) => (
+        <div>
+          <Accordion className="problems-card">
             <Accordion.Toggle as={Card.Header} eventKey="0">
-              <h1 className="week-in-past-problems">Week {week[0]["week"]} Problems</h1>
+              <h1 className="week-in-past-problems">
+                Week
+                {week[0].week}
+                Problems
+              </h1>
             </Accordion.Toggle>
-            
-              <Card>
 
-                <Accordion.Collapse eventKey="0">
-                  <Table striped bordered hover>
-                    <thead>
+            <Card>
+
+              <Accordion.Collapse eventKey="0">
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Problem</th>
+                      <th>Details</th>
+                    </tr>
+                  </thead>
+                  {week.map((problem, index) => (
+                    <tbody>
                       <tr>
-                        <th>#</th>
-                        <th>Problem</th>
-                        <th>Details</th>
+                        <td>{index + 1}</td>
+                        <td>{problem.qName}</td>
+                        <td><ReactMarkdown source={problem.content} /></td>
                       </tr>
-                    </thead>
-                    {week.map((problem, index) => {
-                      return (
-                        <tbody>
-                          <tr>
-                            <td>{index + 1}</td>
-                            <td>{problem["qName"]}</td>
-                            <td><ReactMarkdown source={problem["content"]} /></td>
-                          </tr>
-                        </tbody>
-                      );
-                    })}
+                    </tbody>
+                  ))}
 
-                  </Table>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
+                </Table>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
 
-          </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
